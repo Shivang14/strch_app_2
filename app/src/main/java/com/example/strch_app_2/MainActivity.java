1d0d19053366b85d6d7108a339270e14020bcd94
Shivang14 <shivangkantawala@gmail.com>
2020-06-13 19:38:23 -0400

    Added a new activity for the custom workout page

--- app/src/main/java/com/example/strch_app_2/MainActivity.java
+++ app/src/main/java/com/example/strch_app_2/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        final BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener()
         {
             @Override
@@ -27,9 +27,11 @@ public class MainActivity extends AppCompatActivity
                 switch (item.getItemId()) {
                     case R.id.action_human_body:
                         Toast.makeText(MainActivity.this, "Human Body", Toast.LENGTH_SHORT).show();
+
                         break;
                     case R.id.action_custom_workout:
                         Toast.makeText(MainActivity.this, "Custom Workout", Toast.LENGTH_SHORT).show();
+                        setContentView(R.layout.activity_custom_workout);
                         break;
                     case R.id.action_reports:
                         Toast.makeText(MainActivity.this, "Report", Toast.LENGTH_SHORT).show();

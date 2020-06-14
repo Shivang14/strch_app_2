fe5ea1bbcdfd193cbb1df19740165705bb64f9dd
Shivang14 <shivangkantawala@gmail.com>
2020-06-14 01:54:52 -0400

    Working Bottom Navigation

--- app/src/main/java/com/example/strch_app_2/MainActivity.java
+++ app/src/main/java/com/example/strch_app_2/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.strch_app_2;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -9,6 +12,9 @@ import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationMenu;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import static androidx.navigation.Navigation.findNavController;
+
 
 public class MainActivity extends AppCompatActivity
 {
@@ -18,6 +24,11 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+       NavController navController =(NavController) findNavController(findViewById(R.id.fragment));
+
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
 
     }
+
 }

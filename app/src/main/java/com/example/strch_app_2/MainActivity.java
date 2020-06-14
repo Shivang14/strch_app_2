360a328a71dc8cf1a5491eb32a3645d5cf07a9c5
Shivang14 <shivangkantawala@gmail.com>
2020-06-13 21:39:58 -0400

    Clean Start of the Bottom Navigation

--- app/src/main/java/com/example/strch_app_2/MainActivity.java
+++ app/src/main/java/com/example/strch_app_2/MainActivity.java
@@ -18,27 +18,6 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener()
-        {
-            @Override
-            public void  onNavigationItemReselected(@NonNull MenuItem item)
-            {
-                switch (item.getItemId()) {
-                    case R.id.action_human_body:
-                        Toast.makeText(MainActivity.this, "Human Body", Toast.LENGTH_SHORT).show();
 
-                        break;
-                    case R.id.action_custom_workout:
-                        Toast.makeText(MainActivity.this, "Custom Workout", Toast.LENGTH_SHORT).show();
-                        setContentView(R.layout.activity_custom_workout);
-                        break;
-                    case R.id.action_reports:
-                        Toast.makeText(MainActivity.this, "Report", Toast.LENGTH_SHORT).show();
-                        break;
-                }
-
-            }
-        });
     }
 }
